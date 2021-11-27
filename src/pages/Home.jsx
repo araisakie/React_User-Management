2e781bee918f8e7240a36db54fcea676b39a5b7b
新井沙紀枝 <araisakie@araisakiedanoMacBook-Air.local>
2021-11-27 15:21:57 +0900

    add:dummydata & UserData file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import styled from "styled-components";
 
 import { Layout } from "../component/layout/Layout";
+import { UserData } from "../../src/UserData";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `araisakie${val}`,
+    email: "sakie@test.com",
+    age: "27歳",
+    telephone: "090-1111-2234",
+  };
+});
 
 export const Home = () => {
   return (
     <Layout showFooter>
       <SContainer>
         <SH2>USERS LIST</SH2>
+        <div>
+          {users.map((user) => (
+            <UserData key={user.id} user={user} />
+          ))}
+        </div>
       </SContainer>
     </Layout>
   );
