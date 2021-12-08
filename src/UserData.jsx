911228cd2e9ec91a8e0726b5fadd1fbe649e9443
新井沙紀枝 <araisakie@araisakiedanoMacBook-Air.local>
2021-12-08 18:22:22 +0900

    homepage layout

--- src/UserData.jsx
+++ src/UserData.jsx
@@ -4,16 +4,23 @@ import styled from "styled-components";
 export const UserData = memo((props) => {
   const { user } = props;
   return (
-    <SDl>
-      <dd>{user.name}</dd>
-      <dd>{user.email}</dd>
-      <dd>{user.age}</dd>
-      <dd>{user.telephone}</dd>
-    </SDl>
+    <STr>
+      <STd>{user.name}</STd>
+      <STd>{user.email}</STd>
+      <STd>{user.age}</STd>
+      <STd>{user.telephone}</STd>
+    </STr>
   );
 });
 
-const SDl = styled.dl`
+const STr = styled.tr`
   display: flex;
   justify-content: center;
+`;
+
+const STd = styled.td`
+  text-align: left;
+  width: 185px;
+  display: inline-block;
+  margin: 0 20px 15px 0;
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,37 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 import { Layout } from "../component/layout/Layout";
 import { UserData } from "../../src/UserData";
-import axios from "axios";
-
-const users = [...Array(20).keys()].map((val) => {
-  return {
-    id: val,
-    name: `araisakie${val}`,
-    email: "sakie@test.com",
-    age: "27歳",
-    telephone: "090-1111-2234",
-  };
-});
+import api from "../service/server";
 
 export const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      const res = await api.get("/users");
+      setUsers(res.data);
+    };
+    getUserData();
+  }, []);
+  console.log(users);
+
   return (
-    <Layout showFooter>
+    <Layout>
       <SContainer>
         <SH2>USERS LIST</SH2>
-        <SList>
-          {users.map((user) => (
+        <STitle>
+          <SP>名前</SP>
+          <SP>メールアドレス</SP>
+          <SP>年齢</SP>
+          <SP>電話番号</SP>
+        </STitle>
+        <div>
+          {users?.userData?.map((user) => (
             <UserData key={user.id} user={user} />
           ))}
-        </SList>
+        </div>
       </SContainer>
     </Layout>
   );
@@ -31,6 +39,7 @@ export const Home = () => {
 
 const SContainer = styled.div`
   text-align: center;
+  padding-top: 150px;
 `;
 
 const SH2 = styled.h2`
@@ -38,10 +47,18 @@ const SH2 = styled.h2`
   font-weight: bold;
   font-size: 40px;
   text-shadow: 5px 3px 1px #ffd700;
+  padding-bottom: 50px;
 `;
 
-const SList = styled.div`
-  width: 100%;
-  height: 400px;
-  overflow: scroll;
+const STitle = styled.div`
+  display: flex;
+  margin-bottom: 40px;
+  font-weight: bold;
+  text-align: left;
+  justify-content: center;
+`;
+
+const SP = styled.p`
+  width: 185px;
+  margin-right: 20px;
 `;
